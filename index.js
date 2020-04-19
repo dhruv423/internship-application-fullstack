d69b4b39039f30439f5d65c72e7a8382f2c75a12
dhruv423 <dhruvbhavsar25@hotmail.com>
2020-04-19 18:37:53 -0400

    html for variant

--- index.js
+++ index.js
@@ -18,8 +18,24 @@ addEventListener("fetch", (event) => {
 
 
 async function handleRequest(request) {
-  const json = await getResponseJSON();
-  return new Response(JSON.stringify(json));
+  const init = {
+    method: 'Get',
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+    },
+  }
+
+  const json = await fetchGetHtml(URL);
+  const randomRoute = chooseRandRoute(json);
+  const html = await fetchGetHtml(randomRoute);
+  return new Response(html, init);
+}
+
+
+function chooseRandRoute(json) {
+  const urls = json.variants;
+  const route = Math.floor(Math.random() * urls.length);
+  return urls[route];
 }
 
 
@@ -30,4 +46,41 @@ async function getResponseJSON() {
   const response = await fetch(URL);
   const json = await response.json();
   return json;
+}
+
+
+/**
+ * gatherResponse awaits and returns a response body as a string.
+ * Use await gatherResponse(..) in an async function to get the response body
+ * @param {Response} response to
+ */
+async function gatherResponse(response) {
+  const { headers } = response
+  const contentType = headers.get('content-type')
+
+  if (contentType.includes('application/json')) {
+    const body = await response.json()
+    return body
+  } else if (contentType.includes('application/text')) {
+    const body = await response.text()
+    return body
+  } else if (contentType.includes('text/html')) {
+    const body = await response.text()
+    return body
+  } else {
+    const body = await response.text()
+    return body
+  }
+}
+
+
+/**
+ * fetchGetHtml sends a GET request expecting html
+ * Use await fetchGetHtml(..) in an async function to get the HTML
+ * @param {string} url the URL to send the request to
+ */
+async function fetchGetHtml(url) {
+  const response = await fetch(url)
+  const respBody = await gatherResponse(response)
+  return respBody
 }
